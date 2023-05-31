05fffb178fa17126fac9d6407bb1e3518465d412
fazifs <faz.sadeghi@gmail.com>
2023-05-31 18:19:12 +0100

    first commit

---
+++ demo/src/main/java/com/openhealth/demo/DemoApplication.java
@@ -0,0 +1,54 @@
+package com.openhealth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessagingException;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+	@Bean
+	public MessageChannel mqttInputChannel() {
+			return new DirectChannel();
+	}
+
+	@Bean
+	public MessageProducer inbound() {
+			MqttPahoMessageDrivenChannelAdapter adapter =
+							new MqttPahoMessageDrivenChannelAdapter("tcp://localhost:1883", "testClient",
+																							 "test/message");
+			adapter.setCompletionTimeout(5000);
+			adapter.setConverter(new DefaultPahoMessageConverter());
+			adapter.setQos(1);
+			adapter.setOutputChannel(mqttInputChannel());
+			return adapter;
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "mqttInputChannel")
+	
+	public MessageHandler handler() {
+			return new MessageHandler() {
+
+					@Override
+					public void handleMessage(Message<?> message) throws MessagingException {
+							System.out.println("Door status: " + message.getPayload());
+					}
+
+			};
+	}
+}
